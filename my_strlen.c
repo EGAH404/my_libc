fb1afba800bb8790de162070a5332999a2e0e646
EGAH404 <fabienhage@proton.me>
2026-09-17 10:37:01 +0200

    Ton message décrivant ce que tu as fait

--- my_strlen.c
+++ my_strlen.c
@@ -1,9 +1,13 @@
 #include "my_libc.h"
 
-size_t my_strlen(const char* s) {
-  size_t len = 0;
-  while (s[len] != '\0') {
-    len++;
-  }
-  return len;
+size_t
+my_strlen(const char *s)
+{
+	size_t len = 0;
+
+	while (s[len] != '\0') {
+		len++;
+	}
+
+	return len;
 }
