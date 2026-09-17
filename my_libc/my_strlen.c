e40a37649bf36d7be3748d08602620338d0ac762
EGAH404 <fabienhage@proton.me>
2026-09-17 09:18:05 +0200

    Implémentation de my_strlen

--- my_libc/my_strlen.c
+++ my_libc/my_strlen.c
@@ -1,8 +1,12 @@
-#include <my_libc.h">
+#include "my_libc.h"
 
-	size_t
-	my_strlen(const char *s)
-	 {
-		size_t i;
-		i = 0;
-	}
+size_t
+my_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
