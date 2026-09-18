2c5b3c5d2b050828bc0f08511061fc951d477689
EGAH404 <fabienhage@proton.me>
2026-09-18 16:30:23 +0200

    amen

--- my_strlen.c
+++ my_strlen.c
@@ -3,16 +3,19 @@
 size_t
 my_strlen(const char *s)
 
-{size_t len = 0;
-	while (s[len] != '\0')
-{len++;}
+{
+	size_t len = 0;
+	while (s[len] != '\0') {
+		len++;
+	}
 
-return len;}
+	return len;
+}
 
 int
 main(void)
 {
-        return my_strlen("Hello World!") - 12;
+	return my_strlen("Hello World!") - 12;
 }
 
-//1er code et j'ai deja mal au fesse
+// 1er code et j'ai deja mal au fesse
